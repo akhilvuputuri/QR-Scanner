536c56c9ae25c23fc42eb95ba37537f658803452
ailanthustng <ailanthustng@gmail.com>
2021-08-05 03:06:01 +0800

    made changes

--- mobile-frontend/app/screens/HomeScreen.js
+++ mobile-frontend/app/screens/HomeScreen.js
@@ -2,89 +2,76 @@ import React from 'react';
 import { StyleSheet, View, Text, SafeAreaView } from "react-native";
 import SearchBar from '../components/SearchBar';
 import { IconButton, Button } from 'react-native-paper';
+import colors from '../../assets/colors';
 
 function HomeScreen(props) {
   const [searchQuery, setSearchQuery] = React.useState('');
 
   const onChangeSearch = query => setSearchQuery(query);
 
-
   return (
-    <SafeAreaView
-    style={styles.background}>
-
-      <View
-      style={styles.searchBarContainer}>
-        <View style={{}}>
-
-        </View>
-        <Text style={styles.welcomeText}>Welcome Back Willy!</Text>
-        <View style={{width: 350}}>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.searchbarContainer}>
+        <View style={{ width: 350 }}>
           <SearchBar
             placeholder="Search"
             onChangeText={onChangeSearch}
             value={searchQuery}
           />
         </View>
+      </View>
+      <View style={{ marginTop: 90, flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text style={styles.welcomeText}>Welcome Back Willy!</Text>
         <View style={styles.tapWrapper2}>
           <View style={styles.tap2}>
-            <Text style={{
-                fontSize: 20,
-                color: '#fff',
-                fontFamily: "OpenSans_400Regular"
-                }}>My Savings</Text>
-            <View style={{
-              flexDirection: 'row',
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}>
-              <IconButton icon="cash-usd" color="black" size={40} styles={{
-                flex: 1
-              }}/>
-              <Text style={{
-                flex: 1,
-                fontSize: 30,
-                color: '#fff',
-                fontFamily: "OpenSans_400Regular"
-                }}>
-                  $130
-              </Text>
+            <Text style={styles.subHeaderText}>My Savings</Text>
+            <View 
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            >
+              <IconButton icon="cash-usd" color="black" size={36} styles={{ flex: 1 }}/>
+              <Text style={styles.savingsText}>$130</Text>
             </View>
           </View>
           <View style={styles.verticleLine}></View>
           <View style={styles.tap2}>
-            <Text style={{
-                color: '#fff',
-                fontSize: 20,
-                fontFamily: "OpenSans_400Regular"
-                }}>My Coupons</Text>
-            <View style={{
-              flexDirection: 'row',
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}>
-              <IconButton icon="ticket-percent" color="black" size={40} styles={{
-                flex: 1
-              }}/>
-              <Text style={{
-                color: '#fff',
-                flex: 1,
-                fontSize: 30,
-                fontFamily: "OpenSans_400Regular"
-                }}>
-                  40
-              </Text>
+          <Text style={styles.subHeaderText}>My Coupons</Text>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            >
+              <IconButton icon="ticket-percent" color="black" size={36} styles={{ flex: 1 }}/>
+              <Text style={styles.savingsText}>40</Text>
             </View>
           </View>
-          <Text style={{marginTop: 30, marginLeft: 25, fontSize: 30, color: '#fff',fontFamily: "OpenSans_600SemiBold"}}>Start saving today!</Text>
-          <Text style={{marginTop: 70, marginLeft: 100,
-            ...styles.headerText}}>Categories</Text>
+          <Text
+            style={{
+              marginTop: 30,
+              marginLeft: 25,
+              fontSize: 30,
+              color: '#fff',
+              fontFamily: 'OpenSans_600SemiBold'
+            }}
+          >
+            Start saving today!
+          </Text>
+          <Text style={{
+              marginTop: 70,
+              marginLeft: 100,
+              ...styles.headerText
+            }}
+          >
+            Categories
+          </Text>
         </View>
-        
       </View>
-      <View
-      style={styles.body}>
-
+      <View style={styles.body}>
         <View style={styles.tapWrapper}>
           <View style={styles.tap}>
             <IconButton icon="food" size={50} styles={styles.tap}/>
@@ -120,24 +107,23 @@ function HomeScreen(props) {
 const styles = StyleSheet.create({
   verticleLine: {
     height: '50%',
-    width: 3,
+    width: 2,
     backgroundColor: '#fff',
+    borderRadius: 10,
   },
   body: {
-    // backgroundColor: '#333',
     flex: 1
   },
   headerText: {
     color: '#fff',
     fontSize: 24,
     alignSelf: 'center',
-    fontFamily: "OpenSans_600SemiBold"
+    fontFamily: 'OpenSans_600SemiBold',
   },
   tapWrapper: {
     flexDirection: 'row',
     flexWrap: 'wrap',
     backgroundColor: 'white',
-    marginTop: 10,
     marginLeft: 50,
     marginRight: 50,
     paddingBottom: 20,
@@ -162,39 +148,42 @@ const styles = StyleSheet.create({
     paddingLeft: 50,
     flexDirection: 'row',
     flexWrap: 'wrap',
-    
   },
   tap2: {
-    // backgroundColor: 'white',
     width: 150,
-    height: 100,
     margin: 2,
-    // color: '#fff',
     lineHeight: 100,
     textAlign: 'center',
     fontSize: 30,
     alignItems: 'center',
     justifyContent: 'center',
   },
-  background: {
-    backgroundColor: "#0496FF",
+  subHeaderText: {
+    fontSize: 22,
+    color: '#fff',
+    fontFamily: "OpenSans_600SemiBold",
+  },
+  savingsText: {
+    fontSize: 20,
+    color: '#fff',
+    fontFamily: "OpenSans_400Regular",
+  },
+  container: {
     flex: 1,
     justifyContent: 'center',
+    backgroundColor: colors.dodgerblue,
   },
   welcomeText:{
     color: '#fff',
-    marginTop: 10,
-    marginBottom: 10,
     fontSize: 24,
-    fontFamily: "OpenSans_400Regular",
-    alignSelf: 'flex-start',
-    paddingLeft: 30
+    fontFamily: "OpenSans_600SemiBold",
   },
-  searchBarContainer: {
-    // backgroundColor: "#ffffff",
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+  searchbarContainer: {
+    position: 'absolute',
+    top: 65,
+    left: 30,
+    right: 30,
+    zIndex: 1,
   },
 });
 
