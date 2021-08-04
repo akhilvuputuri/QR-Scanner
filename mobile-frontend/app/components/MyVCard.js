e628347b3cc0dff7e3c6ef1c5ce508fb50dc9a03
Akhil Vuputuri <e0309566@u.nus.edu>
2021-08-04 23:45:51 +0800

    Add my vouchers screen

---
+++ mobile-frontend/app/components/MyVCard.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { StyleSheet, View, Text, Image } from "react-native";
+
+const MyVCard = ({ item, last }) => {
+  const { shopName, voucherName, description, rating, cost, value, imageUrl, expiry } = item;
+
+  // shopName, expiry, value
+  return (
+    <View style={styles.container}>
+      <View style={{flex:1}}>
+        <Text style={{fontSize: 24, fontFamily: 'OpenSans_700Bold'}}>{shopName}</Text>
+      </View>
+      <View style={{flex:1, marginTop: 10}}>
+        <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+        <Text style={{fontSize: 16, fontFamily: 'OpenSans_700Bold'}}>{expiry}</Text>
+        <Text style={{fontSize: 16, fontFamily: 'OpenSans_700Bold'}}>Value: ${value}</Text>
+        </View>
+        
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: 80,
+    width: 350,
+    backgroundColor: '#fff',
+    borderRadius: 8,
+    marginLeft: 32,
+    marginTop: 10,
+    marginBottom: 10,
+    paddingTop: 10,
+    paddingBottom: 0,
+    paddingLeft: 10,
+    paddingRight: 10
+  },
+});
+
+export default MyVCard;
